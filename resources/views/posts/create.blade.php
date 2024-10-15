1f34fd538667addde88127bfa17fcba4ee6b8fe5
LAPTOP-93N1UI83\ignac <1922542@alu.murciaeduca.es>
2024-10-15 17:41:44 +0200

    Mi primer commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,13 @@
+<x-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
+    <h1>{{ __('Create a new Post') }}</h1>
+    <form method="POST" action="{{ route('posts.store') }}">
+        @csrf
+
+        @include('posts.form-fields')
+
+        <br />
+        <button type="submit">{{ __('Send') }}</button>
+        <br />
+    </form>
+    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
+</x-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<x-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
+    <h1>Inicio</h1>
+    <x-slot:sidebar>
+        <p>Home Sidebar</p>
+    </x-slot:sidebar>
+</x-layout>
