0dfae11345621912eb3b3940558ad6ab55c73c37
LAPTOP-93N1UI83\ignac <1922542@alu.murciaeduca.es>
2024-10-17 17:01:03 +0200

    Paso 24, no permitir cambiar, eliminar o añadir posts si no estamos registrados

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 class PostController extends Controller
 {
+    public function _construct()
+    {
+        $this->middleware('auth')->except('index', 'show');
+    }
+
     public function index()
     {
         $posts = DB::table(table:'posts')->get();
