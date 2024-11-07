ae89674a74a34bdef3159c42feef64088ec3c770
David <davinacho039@gmail.com>
2024-11-07 13:22:23 +0100

    Ejercicio 5, Añadir last_name al usuario

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,12 +31,14 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'las_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'las_name' => $request->las_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
