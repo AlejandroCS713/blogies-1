ea6f04a57b6587912ea7f5e603ff759727fb8feb
David <davinacho039@gmail.com>
2024-11-10 22:45:03 +0100

    Mostrar el boton de los posts si hay mas de 10 posts

--- resources/views/components/posts-list.blade.php
+++ resources/views/components/posts-list.blade.php
@@ -63,3 +63,31 @@
     </div>
 
 </div>
+@if($posts->count() > 10)
+<div class="flex items-center justify-center mt-4">
+    <button
+        id="toggle-posts-btn"
+        class="bg-sky-600 text-sky-100 px-4 py-2 rounded-full shadow-lg hover:bg-sky-700 active:bg-sky-800"
+        onclick="togglePosts()"
+    >
+        Ver más posts
+    </button>
+</div>
+@endif
+
+<script>
+    function togglePosts() {
+        const posts = document.querySelectorAll('.post-item');
+        const toggleBtn = document.getElementById('toggle-posts-btn');
+        let isExpanded = toggleBtn.getAttribute('data-expanded') === 'true';
+
+        posts.forEach((post, index) => {
+            if (index >= 30) {
+                post.classList.toggle('hidden', isExpanded);
+            }
+        });
+
+        toggleBtn.setAttribute('data-expanded', !isExpanded);
+        toggleBtn.textContent = isExpanded ? 'Ver más posts' : 'Ver menos posts';
+    }
+</script>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,29 +1,3 @@
 <x-blog-layout meta-title="Blog" meta-description="Descripción de la página del Blog">
     <x-posts-list :metaTitle="'Blog'" :posts="$posts" :showUserPostsButton="true"/>
-    <div class="flex items-center justify-center mt-4">
-        <button
-            id="toggle-posts-btn"
-            class="bg-sky-600 text-sky-100 px-4 py-2 rounded-full shadow-lg hover:bg-sky-700 active:bg-sky-800"
-            onclick="togglePosts()"
-        >
-            Ver más posts
-        </button>
-    </div>
-    <script>
-        function togglePosts() {
-            const posts = document.querySelectorAll('.post-item');
-            const toggleBtn = document.getElementById('toggle-posts-btn');
-            let isExpanded = toggleBtn.getAttribute('data-expanded') === 'true';
-
-            posts.forEach((post, index) => {
-                if (index >= 30) {
-                    post.classList.toggle('hidden', isExpanded);
-                }
-            });
-
-            // Cambiar el estado y el texto del botón
-            toggleBtn.setAttribute('data-expanded', !isExpanded);
-            toggleBtn.textContent = isExpanded ? 'Ver más posts' : 'Ver menos posts';
-        }
-    </script>
 </x-blog-layout>
